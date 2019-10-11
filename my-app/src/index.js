37b000500bb91aafa8f01a3041a60822cb4b364d
Yakushenko Evgeniy <yakushenko.evgeny@gmail.com>
2019-10-11 15:38:53 +0300

    first app

---
+++ my-app/src/index.js
@@ -0,0 +1,21 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function TodoItem(){
+    return (
+        <span>
+            Drink Coffe
+        </span>
+    );
+}
+function TodoList(){
+    return (
+        <ul>
+            <li><TodoItem/></li>
+            <li><TodoItem/></li>
+            <li><TodoItem/></li>
+        </ul>
+    );
+}
+ReactDOM.render(<TodoItem/>, document.getElementById("root"));
